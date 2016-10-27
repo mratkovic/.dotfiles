2cdc8e7433772c861351b8d38d167d2410f0f293
mratkovic <marko.ratkovic93@gmail.com>
2016-10-27 14:31:45 +0200

    tmux removed mouse support, i3 minor updates (volume control, brightness...)

--- .i3/scripts/rename_workspace.sh
+++ .i3/scripts/rename_workspace.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 OLD=$(i3-msg -t 'get_workspaces' | sed -e 's/{"num/\n{"num/g' | grep \"focused\":true | sed -e 's/,"/\n/g' | grep name | cut -d\" -f 3)
 
 NEW=$(zenity --text="Enter new name:" --entry --title="Rename workspace $OLD" --entry-text="$OLD")
