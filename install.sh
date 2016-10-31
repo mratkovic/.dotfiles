c62113f3fea8cf55168fc068c641ec6b9d3bb042
mratkovic <marko.ratkovic93@gmail.com>
2016-10-31 19:08:59 +0100

    Install script (copy sym links) added

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles
+
+# old dotfiles backup directory
+olddir=~/dotfiles_old
+
+# list of files/folders to symlink in homedir
+files=".bashrc .bash_aliases .i3 .vim .vimrc .tmux.conf"
+
+
+######### Installation
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~/ ..."
+mkdir -p $olddir
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory ..."
+cd $dir
+echo -e "\n"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory ..."
+    ln -s $dir/$file ~/$file
+done
+echo "Completed!"
